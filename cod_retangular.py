6bc244d6fa471863f6ecf483b81283f0d976d270
Lilian G <lilian.gimenezt@gmail.com>
2023-12-10 17:31:13 -0300

    Inserção da função de decodificar

--- cod_retangular.py
+++ cod_retangular.py
@@ -10,9 +10,36 @@ def calcular_bits_paridade(mensagem, num_linhas, num_colunas):
     # Retornar os bits de paridade em ordem
     return ''.join(paridade_linhas + paridade_colunas)
 
-mensagem = input("Digite a mensagem binária: ")
+def decodificar_mensagem(mensagem, num_linhas, num_colunas):
+    mensagem_original = ""
+
+    # Remover bits de paridade das linhas
+    bits_mensagem = mensagem[:-num_linhas]
+    
+    # Dividir a mensagem em blocos de acordo com o número de colunas
+    blocos = [bits_mensagem[i:i+num_colunas] for i in range(0, len(bits_mensagem), num_colunas)]
+
+    # Transpor a matriz para decodificar a mensagem original
+    matriz_transposta = [list(x) for x in zip(*blocos)]
+
+    for linha in matriz_transposta:
+        mensagem_original += ''.join(linha)
+
+    return mensagem_original
+
+
+mensagem = input("Digite a mensagem: ")
 num_linhas = int(input("Digite o número de linhas: "))
 num_colunas = int(input("Digite o número de colunas: "))
 
-bits_paridade = calcular_bits_paridade(mensagem, num_linhas, num_colunas)
-print("Bits de paridade: ", bits_paridade)
+opcao = int(input("Selecione uma opção:\n\n1 - Calcular bits de paridade\n2 - Descobrir mensagem original (sem bits de paridade)"))
+
+if opcao == 1:
+
+    bits_paridade = calcular_bits_paridade(mensagem, num_linhas, num_colunas)
+    print("Bits de paridade: ", bits_paridade)
+
+elif opcao == 2:
+
+    mensagem_original = decodificar_mensagem(mensagem, num_linhas, num_colunas)
+    print("A mensagem original é:", mensagem_original)
